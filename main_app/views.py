9154ea28837906960bf7a0c499c9edb6b17a8f2d
Seun Dada <seun.dada777@gmail.com>
2024-05-29 11:08:14 -0400

    reform model for database

--- main_app/views.py
+++ main_app/views.py
@@ -2,20 +2,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-class Bird:
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-birds = [
-  Bird('Tweety', 'canary', 'Yellow and cheerful', 2),
-  Bird('Polly', 'parrot', 'Talks a lot', 5),
-  Bird('Sky', 'blue jay', 'Bright blue and lively', 3),
-  Bird('Robin', 'robin', 'Red-breasted and energetic', 4)
-]
-
 # Create your views here.
 def home(request):
   return HttpResponse(request, 'home.html')
@@ -24,4 +10,5 @@ def about(request):
   return render(request, 'about.html')
 
 def bird_index(request):
+  birds = Bird.objects.all()
   return render(request, 'birds/index.html', { 'birds': birds })
