140f141bb414a7729608e449e1d3358329ea3edb
Seun Dada <seun.dada777@gmail.com>
2024-05-29 11:27:32 -0400

    add details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from .models import Bird
 
 # Create your views here.
 def home(request):
-  return HttpResponse(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -12,3 +11,7 @@ def about(request):
 def bird_index(request):
   birds = Bird.objects.all()
   return render(request, 'birds/index.html', { 'birds': birds })
+
+def bird_detail(request, bird_id):
+  bird = Bird.objects.get(id=bird_id)
+  return render(request, 'birds/detail.html', { 'birds': bird })
