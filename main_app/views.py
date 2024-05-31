17a922345569c70a276049452fb603bbabad432c
Seun Dada <seun.dada777@gmail.com>
2024-05-31 08:36:21 -0400

    update url model and views for the app

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Bird
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Bird, Toy
+from .forms import FeedingForm
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def home(request):
@@ -15,3 +19,45 @@ def bird_index(request):
 def bird_detail(request, bird_id):
   bird = Bird.objects.get(id=bird_id)
   return render(request, 'birds/detail.html', { 'birds': bird })
+
+class BirdCreate(CreateView):
+  model = Bird
+  fields = '__all__'
+
+class BirdUpdate(UpdateView):
+  model = Bird
+  fields = ['species', 'description', 'age']
+
+class BirdDelete(DeleteView):
+  model = Bird
+  success_url = '/birds/'
+
+def add_feeding(request, bird_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the bird_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.bird_id = bird_id
+    new_feeding.save()
+  return redirect('bird-detail', bird_id=bird_id)
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
